79539a28f1ab3efdcd143ffe52cc81c9242dd6fe
fotue2007 <joycemogueo@gmail.com>
2025-11-17 14:52:18 +0100

    ajout d'une ligne au message de b

--- src/Message_b.java
+++ src/Message_b.java
@@ -4,6 +4,7 @@ public class Message_b {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Message du membre B");
+		 System.out.println("Deuxième ligne de B");
 	}
 
 }
